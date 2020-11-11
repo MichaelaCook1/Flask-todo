45ffbfe776b9d4cecd871a7d9bd1ec091a55a283
MichaelaCook1 <MCook1@qa.com>
2020-11-11 20:19:00 +0000

    bug fixes

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from os import getenv
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DB_URI')
-app.config['SECRET_KEY'] = 'its a secret '
+app.config['SECRET_KEY'] =getenv('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 
 db = SQLAlchemy(app)
